162e2053b32af9da508b6fb1f3c130de76672e9f
rubynovich <roman@shipiev.me>
2013-04-12 23:03:30 +0400

    fix: admin can close issue without quality

--- lib/issues_quality_issue_patch.rb
+++ lib/issues_quality_issue_patch.rb
@@ -4,28 +4,30 @@ module IssuesQualityPlugin
   module IssuePatch
     def self.included(base)
       base.extend(ClassMethods)
-      
+
       base.send(:include, InstanceMethods)
-      
+
       base.class_eval do
         unloadable
-        
+
         safe_attributes 'rating_id'
-        
+
         validates_presence_of :rating_id, :if => lambda{ |o|
-          o.status.is_closed? && Setting[:plugin_redmine_issues_quality][:issue_status].to_i == Issue.find(o.id).status_id
+          o.status.is_closed? &&
+            Setting[:plugin_redmine_issues_quality][:issue_status].to_i == Issue.find(o.id).status_id &&
+            !User.current.admin?
         }
-        
+
         belongs_to :rating
-        
+
         if Rails::VERSION::MAJOR >= 3
           scope :eql_field, lambda { |field, item|
             if item.present? && field.present?
               {:conditions => {field => item}}
             end
           }
-          
-          scope :with_rating, lambda { 
+
+          scope :with_rating, lambda {
             {:conditions => ["rating_id IS NOT NULL"]}
           }
         else
@@ -34,18 +36,18 @@ module IssuesQualityPlugin
               {:conditions => {field => item}}
             end
           }
-          
-          named_scope :with_rating, lambda { 
+
+          named_scope :with_rating, lambda {
             {:conditions => ["rating_id IS NOT NULL"]}
-          }        
+          }
         end
-        
+
       end
     end
-      
+
     module ClassMethods
     end
-    
+
     module InstanceMethods
     end
   end
